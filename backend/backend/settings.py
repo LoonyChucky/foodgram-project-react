a11d0b1f6efa4e0db916b82559e1e17c09c1476e
Кирилл Ларцев <loonychucky@yandex.ru>
2024-01-13 07:38:22 +0300

    added allowed hosts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
 
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1:9090', 'localhosh:9090', 'https://kirfoodgram.servebeer.com', 'kirfoodgram.servebeer.com']
 
